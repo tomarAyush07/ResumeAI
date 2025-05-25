356ed4cf5595af39c3661d995a06fec8b056ecfa
Ayush Tomar <ayushsinghtomar184@gmail.com>
2025-05-25 13:13:38 +0530

    update the vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
+  build: {
+    outDir: "dist", // Vercel will look for this folder
+  },
+  base: "/", // Important for routing and assets
   plugins: [
     react(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
